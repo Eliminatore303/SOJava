094a2c93bb48583c8ba0c13a25542754fc6070c4
Eliminatore303 <davidebusato03@libero.it>
2022-10-22 18:05:47 +0200

    added an example about how work the filechooser

---
+++ SO Java/src/view/FileChooser.java
@@ -0,0 +1,83 @@
+package view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.awt.event.ActionEvent;
+
+public class FileChooser extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JButton btnNewButton;
+	private JScrollPane scrollPane;
+	private JTextArea textArea;
+	private JFileChooser fileChooser;
+	private String userDir;
+	private File selectedFile;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FileChooser frame = new FileChooser();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FileChooser() {
+		userDir = System.getProperty("user.home");
+		fileChooser = new JFileChooser(userDir +"/Desktop");
+		//fileChooser.setCurrentDirectory(new File(System.getProperty("user.desktop")));
+		setTitle("FileChooser");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		setVisible(true);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		btnNewButton = new JButton("Choose file");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int result = fileChooser.showOpenDialog(fileChooser);
+				if (result == JFileChooser.APPROVE_OPTION) {
+					selectedFile = fileChooser.getSelectedFile();
+					textArea.setText(""+selectedFile);
+				}
+			}
+		});
+		btnNewButton.setBounds(10, 11, 136, 23);
+		contentPane.add(btnNewButton);
+		
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 45, 414, 141);
+		contentPane.add(scrollPane);
+		
+		textArea = new JTextArea();
+		textArea.setEditable(false);
+		scrollPane.setViewportView(textArea);
+		
+		
+		
+	}
+}
